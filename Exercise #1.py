8155683e426b0d09456d73805c00834ce4eb8e2e
kennethyango <74163177+kennethyango@users.noreply.github.com>
2023-09-06 17:24:01 +0800

    Answer to exercise

---
+++ Exercise #1.py
@@ -0,0 +1,36 @@
+# 5
+# user enters number. print the square of the number
+# , use sep to print in full sentence
+# x = eval(input('Enter a number: '))
+# print('The square of ',x,' is ',x*x,'.',sep='')
+
+# ----6
+# user enters number x, use sep for '-----', print out x, 2x, 3x, 4x, 5x
+# x = eval(input('Enter a number: '))
+# print(1*x,2*x,3*x,4*x,5*x, sep = '-----')
+
+# ----7
+# user enters weight in kg, convert it to lbs
+# 1kg = 2.2lbs
+# weight_kg = eval(input('Enter your weight in kilograms(kg): '))
+# weight_lbs = weight_kg * 2.2
+# print('Your weight in pounds(lbs) is: ', weight_lbs)
+
+# ----8
+# user enter three numbers (different input per number),
+# create variables  total, average
+# print the total and average
+# num1 = eval(input('Enter First Number: '))
+# num2 = eval(input('Enter Second Number: '))
+# num3 = eval(input('Enter Third Number: '))
+# total = num1 + num2 + num3
+# average = total / 3
+# print('The Total the input numbers is: ', total)
+# print('The Average is', average)
+
+
+# ----9
+# total bill = 1000
+# 10% tip
+# tip = 100
+# total bill with tip = 1100
