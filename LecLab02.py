8227ac8cf27a14b37a02c6e143aa47bc4e1e6539
kennethyango <74163177+kennethyango@users.noreply.github.com>
2023-09-13 14:55:47 +0800

    Lecture Laboratory 02 Examples

---
+++ LecLab02.py
@@ -0,0 +1,22 @@
+# FOR LOOP
+# for variable name in range (number of times to repeat):
+#   statement(s) to be repeated
+# Example1
+# for i in range(10):
+#     print("Hello")
+#     print("World")
+
+# Example 2
+# for i in range(3):
+#     num = eval(input("Enter a number: "))
+#     print("The square of your number is, ",num*num)
+# print("The loop is now done.")
+
+
+
+print('A')
+print('B')
+for i in range(5):
+    print('C')
+print('D')
+print('E')
