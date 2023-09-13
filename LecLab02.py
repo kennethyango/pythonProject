0b40e7985fc415dc2c94e945eb2c6f108c1fbf87
kennethyango <74163177+kennethyango@users.noreply.github.com>
2023-09-13 15:31:17 +0800

    Update LecLab02.py

--- LecLab02.py
+++ LecLab02.py
@@ -13,10 +13,52 @@
 # print("The loop is now done.")
 
 
+# Example 3
+# print('A')
+# print('B')
+# for i in range(5):
+#     print('C')
+# print('D')
+# print('E')
 
-print('A')
-print('B')
-for i in range(5):
-    print('C')
-print('D')
-print('E')
+# Example 4
+# print('A')
+# print('B')
+# for i in range(5):
+#     print('C')
+# for i in range(5):
+#     print('D')
+# print('E')
+
+#
+# Example 5
+# for num in range(100):
+#     print('The square of the number ',num+1,' is ',(num+1)**2) #** is for exponentiation
+#
+# normally we use i, j, k for looping variables
+
+
+# Example 6
+# for i in range(10,0,-1):
+#     print(i,end='  ')
+# print(" Time's Up!")
+# sample table:
+#     range(10)       0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+#     range(1, 10)        1, 2, 3, 4, 5, 6, 7, 8, 9
+#     range(3, 7)         3, 4, 5, 6
+#     range(2, 15, 3)    2, 5, 8, 11, 14
+#     range(9, 2, -1)    9, 8, 7, 6, 5, 4, 3
+
+for i in range(1,15,2):
+    print(i*'*  ')
+
+
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+
+
+
