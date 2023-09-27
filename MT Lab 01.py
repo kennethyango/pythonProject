8c01b003be84bd859576381723d3550da21c597c
kennethyango <74163177+kennethyango@users.noreply.github.com>
2023-09-27 16:27:42 +0800

    9 27 23

---
+++ MT Lab 01.py
@@ -0,0 +1,39 @@
+# More Examples
+# #1
+# x = eval(input("value for x: "))
+# y = eval(input("value for y: "))
+# print("A: ",abs(x-y)/(x+y))
+# print("B: ",x**y)
+#
+#
+# #2
+# x = eval(input("Enter First Number: "))
+# y = eval(input("Enter Second Number:"))
+# print(" The distance of the numbers is: ",abs(x-y))
+
+
+# #3
+# import math
+# for i in range(1,21):
+#     print(i, end="   ")
+#     print(round(math.sqrt(i),4))
+
+
+# #4
+# import random
+# x = random.randint(1,10)
+# y = random.randint(1,10)
+# print("First Number: ",x)
+# print("Second Number: ",y)
+# print("Their sum is ", x +y)
+
+
+# #5
+# import random
+# letter = input("Input a Letter: ")
+# num_random = random.randint(1,10)
+# print("Random Number value is: ",num_random)
+# for i in range(num_random):
+#     print(letter, end=" ")
+
+
