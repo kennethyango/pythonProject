5c898a2c38605ca029f6337ec82d9c050b9b5b30
kennethyango <74163177+kennethyango@users.noreply.github.com>
2023-10-11 15:17:14 +0800

    10-11

---
+++ MT Lab 02.py
@@ -0,0 +1,66 @@
+# MT LECTURE 02
+# IF STATEMENTS
+# from random import randint
+# num = randint(1,10)
+# guess = eval(input('Enter your guess: '))
+# if guess==num:
+#     print('You got it')
+# else:
+#     print("Try again later!")
+# print("this is the number computer picked:",num)
+# grade = 90
+# if grade >=90 and grade <100:
+#     print('Your grade is A')
+# if grade = 80-89:
+#     print('Your grade is B')
+
+#
+# grade = eval(input('Enter your score: '))
+# if grade>=90 and grade<100 :
+#     print('A')
+# elif grade>=80 and grade<90:
+#     print('B')
+# elif grade>=70 and grade<80:
+#     print('C')
+# elif grade>=60 and grade<70:
+#     print('D')
+# elif grade<60 and grade>=0:
+#     print('F')
+# else:
+#     print("out of range!")
+
+# MORE EXAMPLES
+# 1
+# INPUT from the USER is LENGTH in CM
+# IF the user enters negative number print INVALID
+# IF NOT convert CM to INCHES, PRINT RESULT
+# 1 inch = 2.54cm
+# num = eval(input("Enter length in Centimeter(cm): "))
+# if num < 0:
+#     print("Invalid Input, you entered a negative number!")
+# else:
+#     print("The equivalent in Inches is: ", num*2.54)
+
+# 2
+# INPUT temp from USER
+# INPUT (Ask user Celsius[C] of Fahrenheit[F]?)
+# Convert to the other UNIT, Display the result
+# temp = eval(input("Enter temperature value: "))
+# unit = input("Celsius[C] of Fahrenheit[F]?")
+# if unit=="C":
+#     print("The equivalent in Fahrenheit is, ",9/5*temp+32)
+# elif unit=="F":
+#     print("The equivalent in Celsius is, ", 5/9*(temp-32))
+# else:
+#     print("Invalid Unit Input")
+
+# 3
+# INPUT temp
+# < -273.15             below abs zero
+# = -273.15             abs zero
+# bet. -273.15 and 0    below freezing
+# 0                     freezing point
+# bet. 0 and 100        normal range
+# 100                   boiling point
+# above 100             above boiling point
+
