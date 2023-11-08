fbb62769bba82bef560fab4dab6bc4226d6d44f2
kennethyango <74163177+kennethyango@users.noreply.github.com>
2023-11-08 17:39:17 +0800

    1

--- MT Lab 03.py
+++ MT Lab 03.py
@@ -1,2 +1,50 @@
 # Midterm Lecture Laboratory #3
 # Strings
+
+# word = input("Enter a string: ")
+# if 'a' not in word:
+#     print("Your string does not contain the letter a")
+
+
+# s = 'Python Programming' # Pythan Programming
+# e = s[:4]+'a'+ s[5:]
+# print(s[:4], end="  ")
+# print("a",end="  ")
+# print(s[5:])
+# print(e)
+
+
+
+
+
+# s = 'POTATO SALAD'
+# for i in range(len(s)):
+#     print((s[i]))
+
+# s = 'POTATO SALAD'
+# for c in s:
+#     print(c)
+
+
+# # String methods
+# s = 'Python Programming'
+# s = s.lower()
+# print(s)
+# s = s.upper()
+# print(s)
+# s = s.replace('P','p')
+# print(s)
+# print(s.count('P'))
+# print(s.index('M'))
+# print(s.isalpha())
+
+# s = "Python_Programming"
+# print(s.isalpha())
+#
+# print("Hello \nWorld")
+
+# print('Item# \t Description \t TOtal')
+
+print("Enter a string of at least 6 characters")
+word = input("Enter here: ")
+print("(a) \tThe length the string \"",word,"\" is ",len(word))
